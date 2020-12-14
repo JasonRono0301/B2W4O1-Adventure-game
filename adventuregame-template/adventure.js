e3ebe66849f08634e11fa3beb38c9f58944348f4
Jason R <99057964@mydavinci.nl>
2020-12-14 13:35:28 +0100

    Heb alle id verbonden met een variabele.

--- adventuregame-template/adventure.js
+++ adventuregame-template/adventure.js
@@ -10,3 +10,32 @@ var gameButtons = document.getElementById('game-buttons');
 var button1 = document.getElementById('button1');
 var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
+
+//Variabele met de getElementById van de inventory item.
+var inventory = document.getElementById('inventoryItem');
+inventory.style.display = "none";
+
+function intro1(){
+    container.style.backgroundImage = "url(img/mainImg.png)";
+    container.style.backgroundRepeat  = "no-repeat";
+        titel.innerHTML = 'THE LOST MASK.';
+        titel.style.paddingLeft = "50px";
+        description.innerHTML = 'Druk op de middelste knop';
+        description.style.paddingLeft = "90px";
+            inventory.style.display = "none";
+        button1.innerHTML = "--->";
+        button2.innerHTML = "klik hier";
+        button3.innerHTML = "<---";
+
+}
+//intro1();//De intro functie.
+
+function intro2(){
+    gameButtons.style.display = "none";
+    titel.innerHTML = "Storyline";
+    description.innerHTML = 
+    "Welkom bij THE LOST GAME een game waar jij de masker van Vigil<br> van zijn die een held was in zijn stad. Maar nu is het jou taak om<br> de stad te beschermen tegen de boeven die jouw volk proberen te beroven.<br>Vigil zijn 2 wapens zijn verstopt in 2 verschillende schuiplaatsen.<br> Maar gevaar zit op de loer.";
+
+}
+
+intro2();
