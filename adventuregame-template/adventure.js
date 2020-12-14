8408a30468fecccf03280136d42b7179da799051
Jason R <99057964@mydavinci.nl>
2020-12-14 12:28:02 +0100

    De game beschrijving uitgetypt in Word.

---
+++ adventuregame-template/adventure.js
@@ -0,0 +1,12 @@
+//Adventure Game 
+//14-12-2020.
+
+//Variabelen met de getElementById van de container,title,description.
+var container = document.getElementById('game-container');
+var titel = document.getElementById('title');
+var description = document.getElementById('description');
+//De variabelen met getElementById van de buttons.
+var gameButtons = document.getElementById('game-buttons');
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
